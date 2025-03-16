491864a1e3f576251ae8299bf1f128a973ab3f2f
HishamElhamayel <elhamayelh@gmail.com>
2025-03-16 16:23:11 +0300

    Added chargewallet to wallet controller

--- server/src/controller/wallet.controller.ts
+++ server/src/controller/wallet.controller.ts
@@ -1,3 +1,4 @@
+import Transaction from "#/models/transaction.model";
 import Wallet from "#/models/wallet.model";
 import { RequestHandler } from "express";
 
@@ -7,7 +8,47 @@ export const getWalletBalance: RequestHandler = async (req, res) => {
 
         const wallet = await Wallet.findOne({ _id: user.wallet });
 
+        if (!wallet) {
+            res.status(400).json({ error: "Wallet not found" });
+            return;
+        }
+
         res.json({ balance: wallet?.balance });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Something went wrong" });
+    }
+};
+
+export const chargeWallet: RequestHandler = async (req, res) => {
+    try {
+        const user = req.user;
+        const wallet = await Wallet.findOne({ _id: user.wallet });
+
+        if (!wallet) {
+            res.status(400).json({ error: "Wallet not found" });
+            return;
+        }
+
+        const amount = req.body.amount;
+        wallet.addFunds(amount);
+
+        const transaction = await Transaction.create({
+            wallet: wallet._id,
+            type: "add",
+            amount,
+            balanceAfterTransaction: wallet.balance,
+        });
+
+        res.json({
+            transaction: {
+                id: transaction._id,
+                // wallet: transaction.wallet,
+                type: transaction.type,
+                amount: transaction.amount,
+                balanceAfterTransaction: transaction.balanceAfterTransaction,
+            },
+        });
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: "Something went wrong" });
--- server/src/routes/wallet.route.ts
+++ server/src/routes/wallet.route.ts
@@ -1,9 +1,10 @@
-import { getWalletBalance } from "#/controller/wallet.controller";
+import { chargeWallet, getWalletBalance } from "#/controller/wallet.controller";
 import { mustAuth } from "#/middleware/auth.middleware";
 import { Router } from "express";
 
 const router = Router();
 
 router.get("/getBalance", mustAuth, getWalletBalance);
+router.post("/chargeWallet", mustAuth, chargeWallet);
 
 export default router;
